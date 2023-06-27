be5691683d6e83662bcf896f85cd8ac19b34feb1
Tim Delesio <tdelesio@gmail.com>
2023-06-27 10:41:24 -0400

    Update Index.vue with some basic information

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,15 +4,15 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
     
-    <h1>Hello, world!</h1>
+    <h1>Wrym Hole</h1>
    
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      You played AssHole, the drinking game, but it's not kid friendly.  We've taken this to the next level and created a game for everyone to enjoy.  
     </p>
 
     <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Order Yours</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">Rules</a>
     </p>
 
   </Layout>
@@ -21,7 +21,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Wrym Hole'
   }
 }
 </script>
