2b4d98a6e0287224ccbf63390106b05ef35f267c
Tim Delesio <tdelesio@gmail.com>
2023-06-27 10:50:16 -0400

    Update Rules.vue

--- src/pages/Rules.vue
+++ src/pages/Rules.vue
@@ -1,1 +1,61 @@
+<template>
+  <Layout>
+    
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="card_back.png" width="135" />
+    
+    <h1>Wrym Hole</h1>
+   
+    <p>
+      There are many variations to the rules of the classic game.  Here is the rules that work the best.
+    </p>
+      
+    <p>
+    SETUP:
 
+Shuffle the deck and deal all of the cards face down to each player.  It’s ok if the number of cards are not even. Each player should sort their cards from lowest to highest number and put matching cards together.  
+
+The player with the 1* goes first for the first round. After that,. the player who goes out first becomes the Dragon Master and gets to lead off the next round.  
+
+OBJECTIVE:
+
+It’s simple: Be the first player to get rid of all your cards and become the Dragon Master where you will have more power and control over the game.  
+After that, keep playing for 2nd, 3rd, 4th place etc. Each spot has an advantage over any players below them. The last place finisher becomes the dreaded Wyrm.
+
+
+      GAME ROLES:
+
+Roles are assigned based on the order players go out in the previous round. There are no  set roles in the first game. 
+Dragon Master: This is the person who went out first in the previous round, and has the most power in the game.  The Dragon Master:                                 -Picks where they want to sit                                     -Goes first in the round.    -Gets to trade their worst 2 cards with the  best 2 cards of the Wyrm.  (If playing with 5 or less players, it’s recommended that only 1 card is exchanged instead of 2.)
+Dragon Trainer: This is the person who went out second in the previous round. The Dragon Trainer: -Gets to select a seat to the left or to the right of the Dragon Master and goes second during a round.  
+
+
+      -Trades their worst card with  the best card of the Outcast (  If playing with 4 or less people, it is not recommended to trade cards.)  
+Wyrm: This is the lowest ranking role in the game. The Wyrm must:                -Shuffle and deal the cards -Clear the cards.             -Trade their best cards with the Dragon Master.         -And will always go last.    -They can also be send to fetch snacks, drinks or other errands by any player in a higher position except while dealing/shuffling.  
+Outcast:  This is the second lowest ranking role in the game.  They sit next to the Wyrm and have to exchange cards with the Dragon Trainer.
+
+Drakes: All the other players after the Dragon Master and Dragon Trainer take  the remaining seats in the order they went out.  
+
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Order Yours</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">Rules</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Wrym Hole'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
